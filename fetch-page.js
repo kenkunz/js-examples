533b1608e65aceae930b59d444c4a7d58c09d051
Ken Kunz <kennethkunz@gmail.com>
2022-03-01 16:57:32 -0600

    update fetchPage to use async/await

--- fetch-page.js
+++ fetch-page.js
@@ -1,14 +1,9 @@
 import axios from 'axios';
 
-export default function fetchPage(url) {
-  return new Promise((resolve, reject) => {
-    const t0 = new Date();
-    axios.get(url)
-      .then(response => {
-        const t1 = new Date();
-        const elapsed = (t1 - t0) / 1000;
-        resolve([ url, response.status, response.data.length, elapsed ]);
-      })
-      .catch(reject);
-  });
+export default async function fetchPage(url) {
+  const t0 = new Date();
+  const response = await axios.get(url);
+  const t1 = new Date();
+  const elapsed = (t1 - t0) / 1000;
+  return([ url, response.status, response.data.length, elapsed ]);
 }
